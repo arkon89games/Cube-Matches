c7478e956c31597b8c3ca309d0ffbd81e83af57f
arkon89games <Arkon89game@gmail.com>
2020-12-25 16:30:10 +0300

    Default Scene Generator

---
+++ Assets/Scripts/Prototype/SceneManager/Editor/SceneGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class SсeneGenerator
+{
+    static SсeneGenerator()
+    {
+        EditorSceneManager.newSceneCreated += OnSceneCreated;
+        // Debug.Log("Hellow!");
+    }
+
+    public static void OnSceneCreated(Scene scene, NewSceneSetup setup, NewSceneMode mode)
+    {
+        var cameraGameObject = Camera.main.transform;
+        var lightGameObject = GameObject.Find("Directional Light").transform;
+
+        var setupFoldier = new GameObject("[SETUP]").transform;
+
+        var lights = new GameObject("Lights").transform;
+        lights.parent = setupFoldier;
+        lightGameObject.parent = lights;
+
+        var cameras = new GameObject("Cameras").transform;
+        cameras.parent = setupFoldier;
+        cameraGameObject.parent = cameras;
+
+        var worldFoldier = new GameObject("[WORLD]").transform;
+        new GameObject("Static").transform.parent = worldFoldier;
+        new GameObject("Dinamic").transform.parent = worldFoldier;
+
+        new GameObject("[UI]");
+
+        Debug.Log("New Scene Created!");
+    }
+}
+
